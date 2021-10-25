347c1ef18262cf6e9200d781161549d6b02d9876
Rangel Reale <rangelspam@gmail.com>
2021-10-25 14:36:59 -0300

    priority type

--- config.go
+++ config.go
@@ -3,12 +3,12 @@ package ecapplog
 type Priority string
 
 const (
-	Priority_TRACE = "TRACE"
-	Priority_DEBUG = "DEBUG"
-	Priority_INFORMATION = "INFORMATION"
-	Priority_NOTICE = "NOTICE"
-	Priority_WARNING = "WARNING"
-	Priority_FATAL = "FATAL"
-	Priority_CRITICAL = "CRITICAL"
-	Priority_ERROR = "ERROR"
+	Priority_TRACE       Priority = "TRACE"
+	Priority_DEBUG                = "DEBUG"
+	Priority_INFORMATION          = "INFORMATION"
+	Priority_NOTICE               = "NOTICE"
+	Priority_WARNING              = "WARNING"
+	Priority_FATAL                = "FATAL"
+	Priority_CRITICAL             = "CRITICAL"
+	Priority_ERROR                = "ERROR"
 )
